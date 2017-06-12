5213ba48f51e5a7e94757dfccfdaac8d1dea183d
umatbro <umatbroo@gmail.com>
2017-06-12 21:27:08 +0200

    adding ints and strings

--- js/w3/src.js
+++ js/w3/src.js
@@ -1,3 +1,9 @@
+var person = "John Doe",
+carName = "VW",
+price = 300;
+
+var test_arithmetic = 2 + 3 + "5"; // = "55"
+
 function dispDemo(text){
   document.getElementById('demo').innerHTML = text;
 }
