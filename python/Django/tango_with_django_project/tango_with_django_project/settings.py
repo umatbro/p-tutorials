b2d2e185a36c33b27e03c0b3b086267451b40baf
umatbro <umatbroo@gmail.com>
2017-07-10 11:40:52 +0200

    django static dir for storing files

--- python/Django/tango_with_django_project/tango_with_django_project/settings.py
+++ python/Django/tango_with_django_project/tango_with_django_project/settings.py
@@ -15,6 +15,8 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+
 
 # tango tutorial - commented out but leave it to see how to do it manually
 # SETTINGS_DIR = os.path.dirname(__file__)
@@ -126,3 +128,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    STATIC_PATH,
+]
