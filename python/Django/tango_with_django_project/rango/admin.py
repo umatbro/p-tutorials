e274c29ede3f44ffbca112eb1d197e2c4b091d7a
umatbro <umatbroo@gmail.com>
2017-07-17 11:51:12 +0200

    django tango_with_django abandon (page 47)

--- python/Django/tango_with_django_project/rango/admin.py
+++ python/Django/tango_with_django_project/rango/admin.py
@@ -2,5 +2,8 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from rango.models import Page, Category
 
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(Page)
--- python/Django/tango_with_django_project/rango/models.py
+++ python/Django/tango_with_django_project/rango/models.py
@@ -10,6 +10,9 @@ class Category(models.Model):
     def __unicode__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Categories"
+
 class Page(models.Model):
     category = models.ForeignKey(Category)
     title = models.CharField(max_length=128)
