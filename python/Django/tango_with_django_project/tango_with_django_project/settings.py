79202a4e2c495f0c03332be71da91bde3ed2b597
umatbro <umatbroo@gmail.com>
2017-07-17 10:30:34 +0200

    django db connection

--- python/Django/tango_with_django_project/tango_with_django_project/settings.py
+++ python/Django/tango_with_django_project/tango_with_django_project/settings.py
@@ -89,6 +89,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'USER': None,
+        'PASSWORD' : None,
+        'HOST' : None,
+        'PORT' : None,
     }
 }
 
