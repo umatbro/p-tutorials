7eed37305bcef1f1b3a364d7091ae36889aa2261
umatbro <umatbroo@gmail.com>
2017-10-16 21:41:09 +0200

    python tuple utils

--- utils/utils.py
+++ utils/utils.py
@@ -1,6 +1,7 @@
 """
 Module containing utility functions
 """
+import operator
 
 
 def remap(x, in_min, in_max, out_min, out_max):
@@ -28,3 +29,28 @@ def constrain(value, max_val, min_val=0):
     """
     return max(min_val, min(value, max_val))
 
+
+def add_tuples(a, b):
+    """
+    Add subsequent elements of a tuple to each other.
+
+    Example: (1, 3, 5) + (3, 3, 3) = (4, 6, 8)
+
+    :param a: first tuple
+    :param b: second tuple
+    :return: result tuple
+    """
+    return tuple(map(operator.add, a, b))
+
+
+def sub_tuples(a, b):
+    """
+    Subtract subsequent elements of a tuple to each other.
+
+    Example: (1, 3, 5) - (3, 3, 3) = (-2, 0, 2)
+
+    :param a: first tuple
+    :param b: second tuple
+    :return: result tuple
+    """
+    return tuple(map(operator.sub, a, b))
