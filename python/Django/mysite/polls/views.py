6d71805a334280f64121bebc2231b69442df2770
umatbro <umatbroo@gmail.com>
2017-10-29 00:29:47 +0200

    Added generic django views

--- python/Django/mysite/polls/views.py
+++ python/Django/mysite/polls/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 from polls.models import Question, Choice
 
@@ -53,3 +54,22 @@ def vote(request, question_id):
         # after successfully dealing with POST data always return HttpResponseRedirect
         # this prevents data to be posted twice if a user hits back button
         return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
+
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
