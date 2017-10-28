2729cc8baf6c806047ca2bb43e0156a56882a839
umatbro <umatbroo@gmail.com>
2017-10-28 23:27:25 +0200

    added voting form

--- python/Django/mysite/polls/models.py
+++ python/Django/mysite/polls/models.py
@@ -6,6 +6,7 @@ from django.db import models
 import datetime
 from django.utils import timezone
 
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -14,15 +15,14 @@ class Question(models.Model):
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
-    #@python_2_unicode_compatible
     def __str__(self):
         return self.question_text
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
-    #@python_2_unicode_compatible
     def __str__(self):
         return self.choice_text
--- python/Django/mysite/polls/views.py
+++ python/Django/mysite/polls/views.py
@@ -3,10 +3,12 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, get_object_or_404
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import loader
+from django.urls import reverse
 
-from polls.models import Question
+from polls.models import Question, Choice
+
 
 # Create your views here.
 def index(request):
@@ -30,9 +32,24 @@ def detail(request, question_id):
 
 
 def results(request, question_id):
-    response = "You're looking at the result of question %s"
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s" % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # redisplay the question voting formpass
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': 'You did not select a choice',
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # after successfully dealing with POST data always return HttpResponseRedirect
+        # this prevents data to be posted twice if a user hits back button
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id, )))
