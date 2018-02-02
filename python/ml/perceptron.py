c1d5c7fc1641e2565b7884a5a37f8f11f2cb6f07
umatbro <umatbroo@gmail.com>
2018-02-02 16:41:19 +0100

    improved perceptron
    it now uses sigmoid function for output
    added some functions and documentation

--- python/ml/perceptron.py
+++ python/ml/perceptron.py
@@ -3,7 +3,7 @@ Based on https://appliedgo.net/perceptron/
 """
 
 import numpy as np
-from typing import Union
+from typing import Union, Callable
 
 from numpy import sign, signbit, heaviside
 
@@ -26,11 +26,12 @@ class Perceptron:
         if len(inputs) != len(self):
             raise ValueError('Input size not valid (expected size: {})'.format(len(self)))
 
-        sum = self.bias
+        result = self.bias
         for input, weight in zip(inputs, self.weights):
-            sum += input * weight
+            result += input * weight
 
-        return int(not signbit(sum))
+        return sigmoid(result)
+        # return int(not signbit(result))
         # return heaviside(sum, self.weights[-1])
 
     def adjust(self, inputs, target, learning_rate):
@@ -39,6 +40,17 @@ class Perceptron:
             self.weights[i] += input * delta * learning_rate
 
         self.bias += delta * learning_rate
+
+    def guess_y(self, x):
+        return self.a * x + self.b
+
+    @property
+    def a(self):
+        return - self.weights[0] / self.weights[1]
+
+    @property
+    def b(self):
+        return - self.bias / self.weights[1]
 
     def __len__(self):
         """
@@ -50,60 +62,88 @@ class Perceptron:
         return 'Weights: {}\nBias: {}'.format(self.weights, self.bias)
 
 
-def f(x, a=None, b=None):
+def f(x: float, a: float=None, b: float=None) -> float:
     """
-    Linear function
+    Linear function.
 
     :param x: argument
     :param a: slope
     :param b: intercept
-    :return: value
+    :return: value being the product of a function
     """
     a = 0.5 if a is None else a
     b = 0.1 if b is None else b
     return a * x + b
 
 
-def is_above_line(x, y, f):
+def is_above_line(x: float, y: float, f: Callable[float, float]) -> bool:
     return y > f(x)
 
 
-def train(perceptron: Perceptron, n_iters: int, f, rate):
+def train(perceptron: Perceptron, n_iters: int, func: Callable[float, float], rate):
     """
     Generate random test points and feed them to perceptron.
     Then compare answer against the solution.
-    :return:
+    Change perceptron weights accordingly using Perceptron.adjust function.
     """
-    print(perceptron)
     for i in range(n_iters):
         point = np.random.rand(2) * 200 - 100
-        # actual = perceptron.output(point)
-        target = int(is_above_line(point[0], point[1], f))
+        target = int(is_above_line(point[0], point[1], func))
 
         perceptron.adjust(point, target, rate)
-    print(perceptron)
 
 
-def verify(perceptron: Perceptron, f):
+def verify(perceptron: Perceptron, func: Callable[float, float]) -> int:
+    """
+    Verify the percentage of correct perceptron answers.
+    Function runs 100 tests and checks correctness.
+
+    :param perceptron: perceptron to be verified
+    :param func: linear function
+    :return: Percentage of correct answers (0 - 100)
+    """
     correct = 0
     for i in range(100):
         point = np.random.rand(2) * 200 - 100
 
         result = perceptron.output(point)
-        if result == is_above_line(point[0], point[1], f):
+        if result == is_above_line(point[0], point[1], func):
             correct += 1
 
     return correct
 
 
-if __name__ == '__main__':
-    a = np.random.uniform(-5, 5)
-    b = np.random.uniform(-50, 50)
-    perc = Perceptron(2)
-    print(perc)
-    iterations = 10000
-    learn_rate = 0.1
-    train(perc, iterations, learn_rate)
-    succ_rate = verify(perc, lambda x: a * x + b)
+def sigmoid(x):
+    """
+    Result calculated as :math:`\frac{1}{1 + e^{-x}}`
 
-    print('{}% succ rate'.format(succ_rate))
+    :return: Computed sigmoid function value
+    """
+    return 1 / (1 + np.e ** (-x))
+
+
+if __name__ == '__main__':
+    # a = np.random.uniform(-5, 5)
+    # b = np.random.uniform(-50, 50)
+    # perc = Perceptron(2)
+    # print(perc)
+    # iterations = 10000
+    # learn_rate = 0.1
+    # f = lambda x: 2 * x + 5
+    # train(perc, iterations, f, learn_rate)
+    # succ_rate = verify(perc, f)
+    #
+    # print('{}% succ rate'.format(succ_rate))
+    #
+    # w0 = perc.weights[0]
+    # w1 = perc.weights[1]
+    # w2 = perc.bias
+    # print('a: {}\tb: {}'.format(-w0/w1, -w2/w1))
+    p = Perceptron(2)
+    func = lambda x: 1 * x + 0
+    print('Before training')
+    print('{}%\na: {}\tb: {}'.format(verify(p, func), p.a, p.b))
+    train(p, 1000, f, 0.9)
+    print('After training')
+    print('{}%\na: {}\tb: {}'.format(verify(p, func), p.a, p.b))
+    print('Guess f(2) = {}'.format(p.guess_y(2)))
--- python/ml/tests_.py
+++ python/ml/tests_.py
@@ -1,7 +1,8 @@
 import unittest
 from unittest import TestCase
+from copy import deepcopy
 
-from ml.perceptron import Perceptron, is_above_line
+from ml.perceptron import Perceptron, is_above_line, train, verify
 
 
 class PerceptronTests(TestCase):
@@ -15,6 +16,52 @@ class PerceptronTests(TestCase):
         self.assertTrue(is_above_line(10, 22, f))
         self.assertFalse(is_above_line(10, 21, f))
         self.assertFalse(is_above_line(10, 20, f))
+
+    def test_adjust(self):
+        p = Perceptron(2)
+        p.weights[0] = 0.5
+        p.weights[1] = 0.5
+        p.bias = 1
+        p1 = deepcopy(p)
+        p2 = deepcopy(p)
+        p3 = deepcopy(p)
+        p4 = deepcopy(p)
+
+        inputs1 = [1, 0]
+        inputs2 = [1, 2]
+        inputs3 = [2, -1]
+        inputs4 = [-2, -1]
+        output1 = 1
+        output2 = 1
+        output3 = 1
+        output4 = 0
+        self.assertAlmostEqual(p1.output(inputs1), output1, places=0)
+        self.assertAlmostEqual(p2.output(inputs2), output2, places=0)
+        self.assertAlmostEqual(p3.output(inputs3), output3, places=0)
+        self.assertAlmostEqual(p4.output(inputs4), output4, places=0)
+
+        # first adjust single
+        p1.adjust(inputs1, 0, 0.1)
+        self.assertAlmostEqual(p1.weights[0], 0.4, places=0)
+        self.assertAlmostEqual(p1.weights[1], 0.5, places=0)
+        self.assertAlmostEqual(p1.bias, 0.9, places=0)
+
+        # then adjust p one by one
+
+    def test_train(self):
+        p = Perceptron(2)
+
+        def func(x):
+            return 2 * x
+        result1 = verify(p, func)
+        train(p, 1000, func, 0.2)
+        result2 = verify(p, func)
+        self.assertGreater(result2, result1)
+
+        print('a: {}\tb: {}'.format(p.a, p.b))
+
+        self.assertAlmostEqual(p.a, 2, places=0)
+        self.assertAlmostEqual(p.b, 0, places=0)
 
 
 if __name__ == '__main__':
