627838b4bb5fccea9a35bda685763b93aecd2243
umatbro <umatbroo@gmail.com>
2018-02-19 18:35:36 +0100

    add helloworld view

--- js/nodejs/express/nodetest/routes/index.js
+++ js/nodejs/express/nodetest/routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', {title: 'Hello world'});
+});
+
 module.exports = router;
