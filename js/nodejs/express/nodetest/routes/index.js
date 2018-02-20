4abd0a8c067e510a8571e1505a1ad321e4d69ca8
umatbro <umatbroo@gmail.com>
2018-02-20 19:57:55 +0100

    adding new user entry functionality

--- js/nodejs/express/nodetest/routes/index.js
+++ js/nodejs/express/nodetest/routes/index.js
@@ -26,4 +26,34 @@ router.get('/userlist', function(req, res) {
   });
 });
 
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add new user'} );
+});
+
+router.post('/adduser', function (req, res) {
+    // set internal db variable
+    let db = req.db;
+
+    // get form values. They rely on "name" attributes
+    let userName = req.body.username;
+    let userEmail = req.body.useremail;
+
+    // set our collection
+    let collection = db.get('usercollection');
+
+    // submit to DB
+    collection.insert({
+        "username": userName,
+        "email" : userEmail
+    }, function(err, doc) {
+        if (err) {
+            // if fail return error
+            res.send("There was a problem adding information to the database");
+        } else {
+            // forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
 module.exports = router;
