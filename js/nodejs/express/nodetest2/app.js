7420662a1a0c081df3ea6c34ea469020601bfcdf
umatbro <umatbroo@gmail.com>
2018-02-24 21:46:02 +0100

    add unittest links, remove old node folder

--- js/nodejs/express/nodetest2/app.js
+++ js/nodejs/express/nodetest2/app.js
@@ -1,14 +1,19 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+let express = require('express');
+let path = require('path');
+let favicon = require('serve-favicon');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+// Db stuff
+let mongo = require('mongodb');
+let monk = require('monk');
+let db = monk('localhost:27017/nodetest2');
 
-var app = express();
+let index = require('./routes/index');
+let users = require('./routes/users');
+
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,12 +27,18 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+app.use(function(req, res, next) {
+  req.db = db;
+  next();
+});
+
 app.use('/', index);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  let err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
@@ -44,3 +55,9 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+app.set('port', process.env.PORT || 3000);
+
+var server = app.listen(app.get('port'), function () {
+    console.log('server listening on port ' + server.address().port);
+});
