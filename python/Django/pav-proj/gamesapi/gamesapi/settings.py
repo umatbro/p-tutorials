fa6aabc0bffb2e849309dc3158a003c3b2c5488c
umatbro <umatbroo@gmail.com>
2018-03-30 15:44:00 +0200

    add new app to settings and create Game model

--- python/Django/pav-proj/gamesapi/gamesapi/settings.py
+++ python/Django/pav-proj/gamesapi/gamesapi/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'games.apps.GamesConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
