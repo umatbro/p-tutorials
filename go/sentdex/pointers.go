cc9a85893cd766df7d9bf0339889a14edf7abccb
umatbro <umatbroo@gmail.com>
2018-07-29 12:44:37 +0200

    golang pointers

---
+++ go/sentdex/pointers.go
@@ -0,0 +1,14 @@
+package main
+import "fmt"
+
+func main() {
+	var x int = 15
+	a := &x
+	fmt.Println(a)
+	fmt.Println(*a)
+	*a = 5
+	fmt.Println(x)
+	*a = *a**a
+	fmt.Println(x)
+	fmt.Println(*a)
+}
