c4a9f8a39c467d39a527cfa95ded4b3ddd826de0
umatbro <umatbroo@gmail.com>
2018-08-02 22:36:58 +0200

    canvas

--- js/canvas/readme.md
+++ js/canvas/readme.md
@@ -1,1 +1,20 @@
 [Tutorial](https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Drawing_shapes)
+
+# Notes
+
+## Drawing rectangles
+
+``canvas`` only supports one primitive shape: rectangle.
+
+There are 3 functions that draw rectangles on the canvas: 
+* ``fillRect(x, y, width, height)`` - draws filled rectangle
+* ``strokeRect(x, y, width, height)`` - draws rectangular outline
+* ``clearRect(x, y, width, height)`` - clears the specified rectangular area making it transparent
+
+## Drawing paths
+
+Functions to use paths:
+* ``beginPath()`` Creates a new path. Once created, future drawing commands are directed into the path and used to build the path up.
+* ``moveTo()`` Moves the starting point of a new sub-path to the (x, y) coordinates.
+* ``lineTo()`` Connects the last point in the subpath to the x, y coordinates with a straight line.
+* [other](https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D#Paths) (like ``quadraticCurveTo`` or ``arc`` and ``ellipse``)
--- js/canvas/src.js
+++ js/canvas/src.js
@@ -1,9 +1,44 @@
-function draw() {
-    let canvas = document.querySelector('#tut');
-    let ctx = canvas.getContext('2d');
-    
-    ctx.fillStyle = 'rgb(200, 0, 0)';
-    ctx.fillRect(10, 10, 50, 50);
-    ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
-    ctx.fillRect(30, 30, 50, 50);
-}
+functions = [
+    function draw() {
+        let canvas = document.querySelector('#tut');
+
+        if (!canvas.getContext) return; // no support for canvaas
+        
+        let ctx = canvas.getContext('2d');
+        
+        ctx.fillStyle = 'rgb(200, 0, 0)';
+        ctx.fillRect(10, 10, 50, 50);
+        ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+        ctx.fillRect(30, 30, 50, 50);
+
+        ctx.fillStyle = 'rgb(0, 0, 0)';
+        ctx.fillRect(90, 50, 50, 50);
+        ctx.clearRect(100, 60, 30, 30);
+        ctx.strokeRect(110, 70, 10, 10);
+    },
+
+    function drawPaths() {
+        let canvas = document.querySelector('#paths');
+        let ctx = canvas.getContext('2d');
+
+        // draw triangle
+        ctx.beginPath();
+        ctx.moveTo(75, 50);
+        ctx.lineTo(100, 75);
+        ctx.lineTo(75, 100);
+        ctx.fill();
+    },
+
+    function drawSmilingFace() {
+        let canvas = document.querySelector('#smiling-face');
+        let ctx = canvas.getContext('2d');
+        let [w, h] = [canvas.width, canvas.height];
+        ctx.beginPath();
+        console.log(w, h);
+        ctx.arc(w/2, h/2, 10, 0, Math.PI * 2, true);
+    }
+]
+
+functions.forEach(func => {
+    document.addEventListener('DOMContentLoaded', func);
+});
