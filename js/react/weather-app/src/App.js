2d574099aea7437c1eae99460f3f5abf88c67fca
umatbro <umatbroo@gmail.com>
2018-09-27 08:33:36 +0200

    display api call results

--- js/react/weather-app/src/App.js
+++ js/react/weather-app/src/App.js
@@ -6,15 +6,35 @@ import Weather from './components/Weather'
 const API_KEY = '8dabfe815233a9733881c391720ab8e3';
 
 class App extends Component {
+  state = {
+    temperature: null,
+    humidity: null,
+    pressure: null,
+    description: null,
+    error: null,
+  }
 
   getWeather = async (event) => {
     event.preventDefault();
     const latitude = event.target.elements.latitude.value;
     const longitude = event.target.elements.longitude.value;
     const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}`);
-   
+    if (!(latitude && longitude)) {
+      return;
+    }
     const data = await api_call.json();
-    console.log(data);
+
+    if (data.code >= 400) {
+      this.setState({error: data.error});
+      return;
+    }
+    this.setState({
+      temperature: data.currently.temperature,
+      humidity: data.currently.humidity,
+      pressure: data.currently.pressure,
+      description: data.currently.summary,
+    });
+
     return data;
   }
 
@@ -23,7 +43,13 @@ class App extends Component {
       <div>
         <Title />
         <Form getWeather={this.getWeather} />
-        <Weather />
+        <Weather 
+          temperature={this.state.temperature} 
+          humidity={this.state.humidity}
+          pressure={this.state.pressure}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
--- js/react/weather-app/src/components/Weather.js
+++ js/react/weather-app/src/components/Weather.js
@@ -1,10 +1,29 @@
 import React from 'react';
+import '../App.css';
 
 class Weather extends React.Component {
     render() {
+        if (this.props.error) {
+            return <h3 className="error">{this.props.error}</h3>
+        }
+
+        const temperature = this.props.temperature;
+        const temperatureMessage = 'Temperature: ' + (temperature !== null ? `${temperature} F` : `-`);
+
+        const humidity = this.props.humidity;
+        const humidityMessage = `Humidity: ${humidity ? humidity + ' %' : '-'}`
+
+        const pressure = this.props.pressure;
+        const pressureMessage = 'Pressure: ' + (pressure !== null ? `${pressure} hPa` : `-`);
+
+        const description = this.props.description ? this.props.description : '';
+
         return (
             <div>
-                Weather Component
+                <h3>{description}</h3>
+                {temperatureMessage}<br />
+                {humidityMessage}<br />
+                {pressureMessage}<br />
             </div>
         );
     }
