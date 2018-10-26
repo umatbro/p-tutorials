4a7dbba3f3842968da8e0d5d99a637d21fd8713e
umatbro <umatbroo@gmail.com>
2018-10-26 22:05:57 +0200

    adding items to the list

--- js/electron/spendings/main.js
+++ js/electron/spendings/main.js
@@ -1,13 +1,11 @@
-//import electron from 'electron';
-//import url from 'url';
-//import path from 'path';
 const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu, ipcMain} = electron;
 
 let mainWindow;
+let addWindow;
 
 // listen for the app to be ready
 app.on('ready', () => {
@@ -18,4 +16,80 @@ app.on('ready', () => {
 	  protocol: 'file:',
 	  slashes: true
 	}));
+
+	// build menu from template
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+	Menu.setApplicationMenu(mainMenu);
+
+	// quit app when closed
+	mainWindow.on('closed', () => {
+		app.quit();
+	});
+
 });
+
+
+// handle create add window
+function createAddWindow() {
+	addWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: 'Shopping list item',
+	});
+	addWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'addWindow.html'),
+		protocol: 'file:',
+		slashes: true
+	}));
+}
+
+// catch item:add
+ipcMain.on('item:add', (event, item) => {
+	mainWindow.webContents.send('item:add', item);
+	addWindow.close();
+});
+
+
+// create menu template
+const mainMenuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Add Item',
+				click() {
+					createAddWindow();
+				}
+			},
+			{
+				label: 'Clear items',
+			},
+			{
+				label: 'Quit',
+				accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+				click() {
+					app.quit();
+				},
+			},
+		]
+	},
+];
+
+// add developer tools if not in prod
+if (process.env.NODE_ENV !== 'production') {
+	mainMenuTemplate.push({
+		label: 'Developer tools',
+		submenu: [
+			{
+				label: 'Toggle DevTools',
+				accelerator: process.platform === 'darwin' ? 'Command+I' : 'Ctrl+I',
+				click(item, focusedWindow) {
+					focusedWindow.toggleDevTools();
+				}
+			},
+			{
+				role: 'reload',
+			}
+		]
+	});
+}
