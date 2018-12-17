19773b8cc03f0407d8464171be68278471902726
umatbro <umatbroo@gmail.com>
2018-12-17 16:23:57 +0100

    Update build_python_from_src.sh
    Add new lib required for python 3.7

--- linux/build_python_from_src.sh
+++ linux/build_python_from_src.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-RELEASE=3.6.3
+RELEASE=3.7.1
  
 # install dependencies
 sudo apt-get update && apt-get upgrade
-sudo apt-get install libbz2-dev liblzma-dev libsqlite3-dev libncurses5-dev libgdbm-dev zlib1g-dev libreadline-dev libssl-dev tk-dev
+sudo apt-get install libbz2-dev liblzma-dev libsqlite3-dev libncurses5-dev libgdbm-dev zlib1g-dev libreadline-dev libssl-dev tk-dev libffi-dev
  
 # download and build Python
 mkdir ~/python3
