43831ec463bd0106b14e1b1866e9d0398e202de2
umatbro <umatbroo@gmail.com>
2021-03-17 20:55:18 +0100

    AWS CDK in Python

---
+++ aws/my-proj/my_proj/my_proj_stack.py
@@ -0,0 +1,24 @@
+from aws_cdk import core as cdk
+import aws_cdk.aws_dynamodb as dynamodb
+
+# For consistency with other languages, `cdk` is the preferred import name for
+# the CDK's core module.  The following line also imports it as `core` for use
+# with examples from the CDK Developer's Guide, which are in the process of
+# being updated to use `cdk`.  You may delete this import if you don't need it.
+from aws_cdk import core
+
+
+class MyProjStack(cdk.Stack):
+    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+        dynamodb.Table(
+            self,
+            "TheTable",
+            table_name="cdk-table",
+            partition_key=dynamodb.Attribute(
+                name="id", type=dynamodb.AttributeType.STRING
+            ),
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+        )
