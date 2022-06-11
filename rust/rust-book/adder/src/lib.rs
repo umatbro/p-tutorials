af5b2b56b3a63267073502862ebf20c10f61ffb4
umatbro <umatbroo@gmail.com>
2022-06-11 20:21:49 +0200

    tests

--- rust/rust-book/adder/src/lib.rs
+++ rust/rust-book/adder/src/lib.rs
@@ -1,5 +1,9 @@
 #[cfg(test)]
 mod tests {
+    use super::Rectangle;
+    use super::add_two;
+    use super::internal_adder;
+
     #[test]
     fn exploration() {
         let result = 2 + 2;
@@ -8,9 +12,47 @@ mod tests {
 
 
     #[test]
-    fn another() {
+    #[should_panic(expected="Make this test fail")]
+    fn should_panic() {
+        // We place the #[should_panic] attribute after the #[test] attribute
+        // and before the test function it applies to.
         panic!("Make this test fail");
     }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle{x: 8, y: 7};
+        let smaller = Rectangle{x: 5, y: 1};
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn internal() {
+        assert_eq!(4, internal_adder(2, 2))
+    }
+
+    #[test]
+    fn it_adds_two() {
+        assert_eq!(4, add_two(2));
+    }
+
+    #[test]
+    fn it_works() -> Result<(), String> {
+        if add_two(2) == 4 {
+            Ok(())
+        } else {
+            Err(String::from("Two plus two does not equal four"))
+        }
+    }
+
+}
+
+pub fn add_two(a: i32) -> i32 {
+    internal_adder(a, 2)
+}
+
+fn internal_adder(a: i32, b: i32) -> i32 {
+    a + b
 }
 
 #[derive(Debug)]
