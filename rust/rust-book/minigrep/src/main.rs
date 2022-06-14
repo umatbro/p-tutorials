cbb8d3208dffb9cd370f07ee227f99803d69c653
umatbro <umatbroo@gmail.com>
2022-06-14 20:08:11 +0200

    Add reading vars and readings the file contents

--- rust/rust-book/minigrep/src/main.rs
+++ rust/rust-book/minigrep/src/main.rs
@@ -1,12 +1,22 @@
 use std::env;
+use std::fs;
 use std::path::Path;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("Args are {:?}", args);
-    let substr: &String = args.get(1).expect("Substring should be provided");
+    let query: &String = args.get(1).expect("Query string should be provided");
     let file: &String = args.get(2).expect("Filename should be provided");
-    if !Path::new(file).exists() {
-        panic!("File {} does not exist", file);
+
+    println!("Searching for {}", query);
+    println!("In file {}", file);
+
+    let file = Path::new(file);
+
+    if !file.exists() {
+        panic!("File {:?} does not exist", file);
     }
+
+    let contents = fs::read_to_string(file).expect("Something went wrong when reading the file.");
+    println!("With text:\n{}", contents);
 }
