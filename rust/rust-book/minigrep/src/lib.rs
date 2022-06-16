96e7c834d802e52f48a4d5bb636fbeadf0862dc5
umatbro <umatbroo@gmail.com>
2022-06-16 09:38:51 +0200

    Add case insensitive function

--- rust/rust-book/minigrep/src/lib.rs
+++ rust/rust-book/minigrep/src/lib.rs
@@ -1,3 +1,4 @@
+use std::ascii::AsciiExt;
 use std::error::Error;
 use std::fs;
 use std::path::Path;
@@ -30,6 +31,65 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     }
 
     let contents = fs::read_to_string(file)?;
-    println!("With text:\n{}", contents);
+
+    for line in search(&config.query, &contents) {
+        println!("{}", line);
+    }
+
     Ok(())
+}
+
+
+fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in content.lines() {
+        if line.contains(query) {
+            result.push(line);
+        }
+    }
+    result
+}
+
+fn serach_case_insensitive<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+    let mut result = Vec::new();
+
+    for line in content.lines() {
+        if line.to_lowercase().contains(&query.to_lowercase()) {
+            result.push(line);
+        }
+    }
+
+    result
+}
+
+// TESTS
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[test]
+    fn test_case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn test_case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            serach_case_insensitive(query, contents)
+        );
+    }
 }
