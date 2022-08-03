9d14a1025229a85d3ef82b5c472243c9814ab104
umatbro <umatbroo@gmail.com>
2022-08-03 19:29:32 +0200

    Use smart pointers to construct conlist

--- rust/rust-book/15_pointers/src/main.rs
+++ rust/rust-book/15_pointers/src/main.rs
@@ -1,13 +1,14 @@
 use crate::List::{Cons, Nil};
 
-
 fn main() {
     let b = Box::new(5);
     println!("b = {}", b);
 
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("List: {:?}", list);
 }
 
+#[derive(Debug)]
 enum List {
     Cons(i32, Box<List>),
     Nil,
