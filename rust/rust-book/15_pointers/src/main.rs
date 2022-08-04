70d7da3a5cfe57383af93c582691e4f7d0861c4f
umatbro <umatbroo@gmail.com>
2022-08-04 18:05:07 +0200

    Linked list init

--- rust/rust-book/15_pointers/src/main.rs
+++ rust/rust-book/15_pointers/src/main.rs
@@ -1,4 +1,6 @@
 use crate::List::{Cons, Nil};
+use std::{ops::Deref, char::MAX};
+use std::iter::Iterator;
 
 fn main() {
     let b = Box::new(5);
@@ -8,7 +10,7 @@ fn main() {
     println!("List: {:?}", list);
 
     let x = 5;
-    let y = &x;
+    let y = Box::new(x);
 
     assert_eq!(5, x);
     assert_eq!(5, *y);
@@ -22,6 +24,10 @@ fn main() {
     let c2 = CustomSmartpointer { data: String::from("qwerty")};
 
     use_list();
+    let z = 6;
+    let my_box = MyBox(z);
+    assert_eq!(6, z);
+    assert_eq!(6, *my_box);
 }
 
 #[derive(Debug)]
@@ -37,6 +43,22 @@ struct CustomSmartpointer {
 impl Drop for CustomSmartpointer {
     fn drop(&mut self) {
         println!("Dropping CustomSmartPointer with data {}", self.data);
+    }
+}
+
+struct MyBox<T>(T);
+
+impl <T> MyBox<T> {
+    fn new(x: T) -> Self {
+        Self(x)
+    }
+}
+
+impl <T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
     }
 }
 
@@ -57,9 +79,10 @@ impl Node {
     }
 }
 
+#[derive(Debug)]
 struct LinkedList<'a> {
-    root: Option<Node>,
-    _curr_iter: Option<&'a Node>,
+    root: Option<Box<Node>>,
+    _curr_iter: Option<&'a Box<Node>>,
 }
 
 impl <'a> LinkedList <'a> {
@@ -69,38 +92,35 @@ impl <'a> LinkedList <'a> {
 
     fn insert(&mut self, value: i32) {
         let new_node= Node::from(value);
-        
         if self.root.is_none() {
-            println!("Root is none");
-            self.root = Some(new_node);
+            self.root = Some(Box::from(new_node));
+            println!("Root has been set to {:?}", self.root);
             return;
         }
-        let mut current = &mut self.root.as_ref().unwrap().next;
-        while current.is_some() {
-            let mut current = &current.as_ref().unwrap().next;
+        // let mut root = self.root.as_mut().unwrap();
+        let mut current = self.root.as_mut().unwrap();
+        println!("first current is {:?}", current);
+        while current.next.is_some() {
+            current = current.next.as_mut().unwrap();
+            println!("New current is {:?}", current);
         }
-        *current = Some(Box::new(new_node));
-        // println!("Root is {}", self.root.as_ref().unwrap().value);
-        let mut root_node = self.root.as_mut().unwrap();
-        // let mut curr = self.root.as_ref();
-        // let curr = root_node.next;
-    //     // println!("First next: {:?}", next);
-    //     if next.is_none() {
-    //        *next.unwrap() = &Some(Box::from(new_node)); 
-    //     }
-        while true {
-            if root_node.next.is_none() {
-                // root_node.set_next(&new_node);
-            }
-        }
-    //     while !next.is_some() {
-    //         next = &next.as_ref().unwrap().next;
-    //         println!("Next value is {:?}", next);
-    //     }
-    //     println!("No more next value ({:?})", next);
-    //     next = &Some(Box::from(new_node));
+        current.next = Some(Box::from(new_node));
     }
 }
+
+impl<'a> Iterator for LinkedList<'a> {
+    type Item = i32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.root.is_none() {
+            return None;
+        }
+        None
+        // self._curr_iter = Some(self.root.as_deref().unwrap());
+        // Some(self.root.as_ref().expect("Root must be set").value)
+    }
+}
+
 
 fn use_list() {
     let mut list = LinkedList::new();
@@ -110,9 +130,15 @@ fn use_list() {
     child.set_next(x);
     parent.set_next(child);
     println!("Parent: {:?}", parent);
-    // list.insert(2);
-    // list.insert(3);
+    list.insert(69);
+    list.insert(2137);
+    list.insert(1338);
+    list.insert(42);
+    println!("Linkded list: {:?}", list);
     // list.insert(6);
+    for x in list {
+        println!("List item: {}", x);
+    }
 }
 
 #[test]
