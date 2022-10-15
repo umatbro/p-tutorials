688c5347a5e6b86e60d01646c9092e682d00cc3c
umatbro <umatbroo@gmail.com>
2022-10-15 13:09:37 +0200

    Macros!

--- rust/rust-book/19_hello_macro/hello_macro_derive/src/lib.rs
+++ rust/rust-book/19_hello_macro/hello_macro_derive/src/lib.rs
@@ -1,4 +1,7 @@
 use proc_macro::TokenStream;
+use syn::DeriveInput;
+use quote::quote;
+
 
 #[proc_macro_derive(HelloMacro)]
 pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
@@ -6,6 +9,19 @@ pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
     // that we can manipulate
     let ast = syn::parse(input).unwrap();
     impl_hello_macro(&ast)
+}
+
+fn impl_hello_macro(ast: &DeriveInput) -> TokenStream {
+    let name = &ast.ident;
+
+    let gen = quote! {
+        impl HelloMacro for #name {
+            fn hello_macro() {
+                println!("Hello, Macros! My name is {}!", stringify!(#name));
+            }
+        }
+    };
+    gen.into()
 }
 
 #[cfg(test)]
