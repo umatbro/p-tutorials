1a9c600a930c67ca85426dcf64ca1d97d3f9488e
umatbro <umatbroo@gmail.com>
2022-10-15 11:18:49 +0200

    Hello macro

---
+++ rust/rust-book/19_hello_macro/hello_macro_derive/src/lib.rs
@@ -0,0 +1,18 @@
+use proc_macro::TokenStream;
+
+#[proc_macro_derive(HelloMacro)]
+pub fn hello_macro_derive(input: TokenStream) -> TokenStream {
+    // Construct a representation of Rust code as a syntax tree
+    // that we can manipulate
+    let ast = syn::parse(input).unwrap();
+    impl_hello_macro(&ast)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
