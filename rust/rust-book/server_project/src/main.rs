db955ed59e2d4a6300d328d01057485d87aaf1ff
umatbro <umatbroo@gmail.com>
2022-10-27 20:06:49 +0200

    Sync server

--- rust/rust-book/server_project/src/main.rs
+++ rust/rust-book/server_project/src/main.rs
@@ -1,8 +1,10 @@
 use std::{
-    net::{TcpListener, TcpStream},
+    fs,
     io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
 };
 
+const CRLF: &str = "\r\n";
 
 fn main() {
     let listener = TcpListener::bind("0.0.0.0:7878").unwrap();
@@ -13,13 +15,28 @@ fn main() {
     }
 }
 
-
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
-    println!("HTTP Request: {:#?}", http_request);
+    //    let http_request: Vec<_> = buf_reader
+    //        .lines()
+    //        .map(|result| result.unwrap())
+    //        .take_while(|line| !line.is_empty())
+    //        .collect();
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+
+    let lines: Vec<String> = vec![
+        String::from(status_line),
+        String::from(format!("Content-Length: {length}{CRLF}")),
+        contents,
+        String::from(CRLF),
+    ];
+
+    stream.write_all(lines.join(CRLF).as_bytes()).unwrap();
 }
