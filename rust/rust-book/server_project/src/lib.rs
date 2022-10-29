a90518099af901a948d6de95002d32b63bed2467
umatbro <umatbroo@gmail.com>
2022-10-29 18:41:49 +0200

    Async version of server

--- rust/rust-book/server_project/src/lib.rs
+++ rust/rust-book/server_project/src/lib.rs
@@ -1,5 +1,4 @@
-use std::thread;
-
+use std::{sync::{mpsc, Arc, Mutex}, thread};
 
 struct Worker {
     id: usize,
@@ -7,15 +6,23 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Self {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, reciver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(move || loop {
+            let job = reciver.lock().expect("Error obtaining lock").recv().unwrap();
+            println!("Worker {id} got a job! Executing...");
+            job();
+            println!("Worker {id} execution finished.");
+        });
 
         Self { id, thread }
     }
 }
 
+type Job = Box<dyn FnOnce() + Send + 'static>;
+
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>
 }
 pub struct PoolCreationError(pub &'static str);
 
@@ -29,13 +36,17 @@ impl ThreadPool {
     /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> Self {
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for i in 0..size {
             // create some threads and store them in the vector
-            workers.push(Worker::new(i))
+            workers.push(Worker::new(i, Arc::clone(&receiver)))
         }
-        Self { workers }
+        Self { workers, sender }
     }
 
     pub fn build(size: usize) -> Result<Self, PoolCreationError> {
@@ -49,5 +60,7 @@ impl ThreadPool {
     pub fn execute<F>(&self, f: F)
     where F: FnOnce() + Send + 'static
     {
+        let job = Box::new(f);
+        self.sender.send(job).unwrap();
     }
 }
