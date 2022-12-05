41147822bfc93f276c39a0e1c7445629a5e74c59
umatbro <umatbroo@gmail.com>
2022-12-06 00:28:31 +0100

    Day5 done

--- rust/advent-of-code/day5/src/main.rs
+++ rust/advent-of-code/day5/src/main.rs
@@ -18,7 +18,7 @@ fn print_stacks(stacks: &Vec<Vec<char>>) {
 }
 
 fn main() {
-    let stacks = vec![
+    let mut stacks = vec![
         vec![],
         vec!['S', 'T', 'H', 'F', 'W', 'R'],
         vec!['S', 'G', 'D', 'Q', 'W'],
@@ -30,23 +30,56 @@ fn main() {
         vec!['N', 'G', 'M', 'T', 'C', 'J', 'R'],
         vec!['L', 'G', 'B', 'W'],
     ];
-    print_stacks(&stacks);
+    let mut stacks_part_2 = stacks.clone();
+
     let path = Path::new("input");
     let file = File::open(path).unwrap();
     let reader = BufReader::new(file);
     for line in reader.lines() {
         let l = line.unwrap();
-        let s = StackTransfer::from_str(&l).unwrap();
-        println!("{:?}", s);
-    }
+        let s = l.parse::<StackTransfer>().unwrap();
+        
+        for _ in 0..s.amount {
+            let mut el = None;
+            let source_stack = &mut stacks[s.from as usize];
+            el = source_stack.pop();
+            if let Some(v) = el {
+                let target_stack = &mut stacks[s.to as usize];
+                target_stack.push(v);
+            } else {
+                panic!("The stack should not be empty!")
+            }
+        }
 
+        // PART 2
+        let source_stack = &mut stacks_part_2[s.from as usize];
+        let amount: usize = s.amount as usize;
+        let elements_to_move = source_stack[source_stack.len()-amount..].to_vec();
+        *source_stack = source_stack[..source_stack.len()-amount].to_vec();
+        let target_stack = &mut stacks_part_2[s.to as usize];
+        target_stack.extend(elements_to_move);
+        
+    }
+    let mut result1 = String::new();
+    for stack in stacks.iter().skip(1) {
+        let c = stack.last().unwrap().clone();
+        result1.push(c);
+    }
+    println!("Result 1: {result1}");
+
+    let mut result2 = String::new();
+    for stack in stacks_part_2.iter().skip(1) {
+        let c = stack.last().unwrap().clone();
+        result2.push(c);
+    }
+    println!("Result 2: {result2}");
 }
 
 #[derive(Debug)]
 struct StackTransfer {
+    amount: u32,
     from: u32,
     to: u32,
-    amount: u32,
 }
 fn parse_input(i: &str) -> IResult<&str, StackTransfer> {
     let (i, _) = nom::bytes::complete::tag("move")(i)?;
@@ -56,10 +89,11 @@ fn parse_input(i: &str) -> IResult<&str, StackTransfer> {
     let (i, _) = nom::bytes::complete::tag("from")(i)?;
     let (i, _) = nom::character::complete::space1(i)?;
     let (i, from) = nom::combinator::map_res(nom::character::complete::digit1, u32::from_str)(i)?;
-
+    let (i, _) = nom::bytes::complete::tag(" to ")(i)?;
+    let (i, to) = nom::combinator::map_res(nom::character::complete::digit1, u32::from_str)(i)?;
     Ok((i, StackTransfer {
         from,
-        to: 0,
+        to,
         amount,
     }))
 }
